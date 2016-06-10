4205da7e6828649bd367510d66131a36d9af9141
AeroDME <AeroDME@gmail.com>
2016-06-09 20:17:16 -0500

    Update eval.f
    Added variable substitution.  Now reads from stdin.

--- eval.f
+++ eval.f
@@ -3,11 +3,13 @@
 * D. Everhart
 * 05 JUN 2016
 ************************************************************************
-* So far, this only evaluates basic math operations (*,/,+,-,^).  It
-* also handles parenthesis grouping "()".  You may ask, "Why did
-* you do it in FORTRAN 77?"  Well, that is a good question. I guess
-* if I can do it in F77, then the algorithm can be adapted to pretty
-* much any language.
+* So far, this only evaluates basic math operations (*,/,+,-).  It even
+* handles parenthesis and substitutes values in for identifiers.  It
+* does not handle functions.... so, there's that.
+*
+* You may ask, "Why did you do it in FORTRAN 77?"  Well, that is a good
+* question. I guess if I can do it in F77, then the algorithm can be
+* adapted to pretty much any language.
 ************************************************************************
 * The MIT License (MIT)
 * 
@@ -34,11 +36,24 @@
 ************************************************************************
       PROGRAM EVAL
       IMPLICIT NONE
-      CHARACTER*64 RAW
-      PARAMETER (RAW=' .5*(1/12)*.05**3')
+      INTEGER SZ
+      PARAMETER (SZ=64)
+      CHARACTER*(SZ) RAW
+      DO 1001 WHILE (.TRUE.)
+      READ(5,9001,END=8999)RAW
+      CALL UCASE(RAW)
+      IF (TRIM(RAW).EQ.'P') THEN
+        CALL PVARS(6)
+        CYCLE
+      ELSE IF (INDEX(RAW,'=').GT.0) THEN
+        CALL SETPAR(RAW)
+        CYCLE
+      END IF
       CALL SCAN(RAW)
       CALL PARSE(RAW)
+ 1001 CONTINUE
  8999 GOTO 9999
+ 9001 FORMAT(A64)
  9999 STOP
       END PROGRAM EVAL
 ************************************************************************
@@ -50,7 +65,7 @@
       PARAMETER (NCLS=5,   NRWS=135,   MTOK=128)
 *
       INTEGER    UNKN,      WHIT,      IDEN      
-      PARAMETER (UNKN=0,    WHIT=101,  IDEN=201)
+      PARAMETER (UNKN=0,    WHIT=101,  IDEN=200)
       INTEGER    INTG,      FLOT,       SCI,      SCIS,      NUMB
       PARAMETER (INTG=301,  FLOT=302,   SCI=303,  SCIS=304,  NUMB=300)
       INTEGER     POW,       MUL,       DIV,      FDIV,      MODU
@@ -59,6 +74,8 @@
       PARAMETER ( ADD=406,   SUB=407,  OPER=400)
       INTEGER     GRP,      OGRP,      CGRP,      OPAR,      CPAR
       PARAMETER ( GRP=500,  OGRP=510,  CGRP=520,  OPAR=511,  CPAR=521)
+      INTEGER    FUNC
+      PARAMETER (FUNC=600)
 *
       INTEGER     ERR,       NEW,      PUSH,      FLSH
       PARAMETER ( ERR=0,     NEW=101,  PUSH=102,  FLSH=103)
@@ -68,6 +85,7 @@
 * ----------------------------------------------------------------------
       INTEGER I,J,NS,NQ,S(3,MTOK),Q(3,MTOK),IVAL(4)
       DOUBLE PRECISION FVAL(2)
+      CHARACTER*128 SVAL
       EQUIVALENCE (FVAL,IVAL)
 *
       FVAL = 0D0
@@ -78,7 +96,7 @@
       DO 1001 I=1,NTOK
       SELECT CASE(100*(TOKLST(1,I)/100))
 *
-      CASE (NUMB)
+      CASE (IDEN,NUMB)
         NQ = NQ + 1
         Q(1,NQ) = TOKLST(1,I)
         Q(2,NQ) = TOKLST(2,I)
@@ -114,11 +132,17 @@
       CASE (GRP)
         SELECT CASE (TOKLST(1,I))
         CASE (OPAR)
+****      If an identifier preceded this open parenthesis, then it
+****      is a function identifier.
+          IF (I.GT.1) THEN
+            IF (TOKLST(1,I-1).EQ.IDEN) THEN
+              Q(1,NQ) = FUNC
+            ENDIF
+          END IF
           NS = NS + 1
           S(1,NS) = TOKLST(1,I)      ! PUSH OPEN PARENTHESIS ONTO STACK
           S(2,NS) = TOKLST(2,I)
           S(3,NS) = TOKLST(3,I)
-          PRINT*,'PUSHING OPAR ONTO STACK'
         CASE (CPAR)
           DO 1021 WHILE (NS.GT.0)
           IF (S(1,NS).EQ.OPAR) THEN
@@ -132,7 +156,6 @@
             NS = NS - 1
           END IF
  1021     CONTINUE
-          PRINT*,'FLUSHING STACK TO OPAR'
         END SELECT
 *
       CASE DEFAULT
@@ -153,12 +176,26 @@
       WRITE(6,9021) (Q(J,I),J=1,3),STR(Q(2,I):Q(3,I))
 **    WRITE(6,9031) NS
       SELECT CASE(100*(Q(1,I)/100))
+*
+      CASE (IDEN)
+        CALL GETVAL(STR(Q(2,I):Q(3,I)),SVAL)
+        IF (LEN_TRIM(SVAL).EQ.0) THEN
+          WRITE(0,9041)STR(Q(2,I):Q(3,I))
+          GOTO 9999
+        END IF
+        READ(SVAL,*)FVAL(1)
+        NS = NS + 1
+        S(1,NS) = Q(1,I)
+        S(2,NS) = IVAL(1)
+        S(3,NS) = IVAL(2)
+*
       CASE(NUMB)
         READ(STR(Q(2,I):Q(3,I)),*)FVAL(1)
         NS = NS + 1
         S(1,NS) = Q(1,I)
         S(2,NS) = IVAL(1)
         S(3,NS) = IVAL(2)
+*
       CASE(OPER)
         IVAL(3) = S(2,NS)
         IVAL(4) = S(3,NS)
@@ -182,6 +219,7 @@
         END SELECT
         S(2,NS) = IVAL(1)
         S(3,NS) = IVAL(2)
+*
       CASE DEFAULT
         WRITE(0,9001) STR(Q(2,I):Q(3,I))
         GOTO 9999
@@ -201,6 +239,7 @@
  9001 FORMAT('0*** ERROR TOKEN: -->',A,'<--')
  9021 FORMAT(3(2X,I4),2X,A)
  9031 FORMAT('0***   STACKSIZE: ',I4)
+ 9041 FORMAT('0*** UNDEFINED VARIABLE: ',A)
  9101 FORMAT('0***      RESULT: ',E12.6)
  9999 STOP 'ERROR IN PARSE'
       END SUBROUTINE PARSE
@@ -215,7 +254,7 @@
       PARAMETER (NCLS=5,   NRWS=135,   MTOK=128)
 *
       INTEGER    UNKN,      WHIT,      IDEN      
-      PARAMETER (UNKN=0,    WHIT=101,  IDEN=201)
+      PARAMETER (UNKN=0,    WHIT=101,  IDEN=200)
       INTEGER    INTG,      FLOT,       SCI,      SCIS,      NUMB
       PARAMETER (INTG=301,  FLOT=302,   SCI=303,  SCIS=304,  NUMB=300)
       INTEGER     POW,       MUL,       DIV,      FDIV,      MODU
@@ -224,6 +263,8 @@
       PARAMETER ( ADD=406,   SUB=407,  OPER=400)
       INTEGER     GRP,      OGRP,      CGRP,      OPAR,      CPAR
       PARAMETER ( GRP=500,  OGRP=510,  CGRP=520,  OPAR=511,  CPAR=521)
+      INTEGER    FUNC
+      PARAMETER (FUNC=600)
 *
       INTEGER     ERR,       NEW,      PUSH,      FLSH
       PARAMETER ( ERR=0,     NEW=101,  PUSH=102,  FLSH=103)
@@ -322,7 +363,7 @@
       PARAMETER (NCLS=5,   NRWS=135,   MTOK=128)
 *
       INTEGER    UNKN,      WHIT,      IDEN      
-      PARAMETER (UNKN=0,    WHIT=101,  IDEN=201)
+      PARAMETER (UNKN=0,    WHIT=101,  IDEN=200)
       INTEGER    INTG,      FLOT,       SCI,      SCIS,      NUMB
       PARAMETER (INTG=301,  FLOT=302,   SCI=303,  SCIS=304,  NUMB=300)
       INTEGER     POW,       MUL,       DIV,      FDIV,      MODU
@@ -331,6 +372,8 @@
       PARAMETER ( ADD=406,   SUB=407,  OPER=400)
       INTEGER     GRP,      OGRP,      CGRP,      OPAR,      CPAR
       PARAMETER ( GRP=500,  OGRP=510,  CGRP=520,  OPAR=511,  CPAR=521)
+      INTEGER    FUNC
+      PARAMETER (FUNC=600)
 *
       INTEGER     ERR,       NEW,      PUSH,      FLSH
       PARAMETER ( ERR=0,     NEW=101,  PUSH=102,  FLSH=103)
@@ -492,4 +535,242 @@
 *                                                                     
      &            99999, 99999, 99999,   ERR,   UNKN                  /
       END BLOCK DATA ASCTAB
+************************************************************************
+      BLOCK DATA BDVARS
+*     ------------------------------------------------------------------
+      INTEGER*4 MAXVCNT, MAXNSZ, MAXVSZ, VCNT
+      PARAMETER (MAXVCNT=128, MAXNSZ=8, MAXVSZ=128)
+      CHARACTER*(MAXNSZ) NAMES(MAXVCNT)
+      CHARACTER*(MAXVSZ) VALUES(MAXVCNT)
+      COMMON /VARS/ VCNT, NAMES, VALUES
+*     ------------------------------------------------------------------
+      DATA       VCNT,         NAMES,         VALUES /
+     1              0,    MAXVCNT*'',     MAXVCNT*'' /
+
+      END BLOCK DATA BDVARS
+************************************************************************
+* PVARS: Print table of VARiableS.
+************************************************************************
+* UNT - I - File unit number to which to write data.
+************************************************************************
+* Prints variables to desired output unit.
+************************************************************************
+      SUBROUTINE PVARS(UNT)
+      IMPLICIT NONE
+*     ------------------------------------------------------------------
+      INTEGER*4 MAXVCNT, MAXNSZ, MAXVSZ, VCNT
+      PARAMETER (MAXVCNT=128, MAXNSZ=8, MAXVSZ=128)
+      CHARACTER*(MAXNSZ) NAMES(MAXVCNT)
+      CHARACTER*(MAXVSZ) VALUES(MAXVCNT)
+      COMMON /VARS/ VCNT, NAMES, VALUES
+*     ------------------------------------------------------------------
+      INTEGER*4 I, UNT
+      WRITE(UNT,197)
+  197 FORMAT('VARABLE',8X,'VALUE')
+      DO 200 I=1,VCNT
+      WRITE(UNT,199) NAMES(I),VALUES(I)
+  199 FORMAT(A8,8X,A64)
+  200 CONTINUE
+      END SUBROUTINE PVARS
+************************************************************************
+* VARIND: Gets VARiable INDex based on name.
+************************************************************************
+* NAME - CH - Name to locate in NAMES() array.
+* Returns the index of NAME if it exists, otherwise, 0.
+************************************************************************
+      INTEGER*4 FUNCTION VARIND(NAME)
+      IMPLICIT NONE
+      CHARACTER*(*) NAME
+*     ------------------------------------------------------------------
+      INTEGER*4 MAXVCNT, MAXNSZ, MAXVSZ, VCNT
+      PARAMETER (MAXVCNT=128, MAXNSZ=8, MAXVSZ=128)
+      CHARACTER*(MAXNSZ) NAMES(MAXVCNT)
+      CHARACTER*(MAXVSZ) VALUES(MAXVCNT)
+      COMMON /VARS/ VCNT, NAMES, VALUES
+*     ------------------------------------------------------------------
+      CHARACTER*(MAXNSZ) UN, CN
+      INTEGER*4 I
+      VARIND = 0
+      UN = ADJUSTL(NAME)
+      CALL UCASE(UN)
+      DO 100 I=1,VCNT
+      CN=NAMES(I)
+      IF (CN(1:LEN_TRIM(CN)).EQ.UN(1:LEN_TRIM(UN))) THEN
+        VARIND = I
+        EXIT
+      END IF
+  100 CONTINUE
+  999 RETURN
+      END FUNCTION VARIND
+************************************************************************
+* SETVAL: SET a name/value pair VARiable (Character).
+************************************************************************
+* NAME  - I - Variable name
+* VALUE - I - Variable value
+************************************************************************
+* Sets variable value.  Variable names are case INSENSITIVE.  Variable
+* values are not altered by case.  Values are, adjusted left.
+************************************************************************
+*  SYMBOL  * TYPE *                DESCRIPTION
+************************************************************************
+*       I  *  I4  * 
+*     IND  *  I4  *
+*  GETIND  *  I4  *
+*       N  * CHR  *
+*       V  *  I4  *
+************************************************************************
+      SUBROUTINE SETVAL(NAME, VALUE)
+      IMPLICIT NONE
+*     ------------------------------------------------------------------
+      INTEGER*4 MAXVCNT, MAXNSZ, MAXVSZ, VCNT
+      PARAMETER (MAXVCNT=128, MAXNSZ=8, MAXVSZ=128)
+      CHARACTER*(MAXNSZ) NAMES(MAXVCNT)
+      CHARACTER*(MAXVSZ) VALUES(MAXVCNT)
+      COMMON /VARS/ VCNT, NAMES, VALUES
+*     ------------------------------------------------------------------
+      CHARACTER*(*) NAME, VALUE
+      INTEGER*4 I, IND, VARIND
+      CHARACTER*(MAXNSZ) N
+      CHARACTER*(MAXVSZ) V
+*
+**  FIRST, check to see if we have exceeded out bounds.
+*
+      IF (VCNT.EQ.MAXVCNT) THEN
+        WRITE(0,999) 'Maximum variable count reached: ', MAXVCNT
+        WRITE(0,998) 'Cannot write variable: ', NAME(:LEN_TRIM(NAME))
+      END IF
+*
+** Condition values.
+*
+      N = ADJUSTL(NAME)
+      V = ADJUSTL(VALUE)
+      CALL UCASE(N)
+*
+**  Get index of NAME if it already exists.
+*     
+      IND = VARIND(N)
+*
+** If index was not found, then increment and set value.
+** Otherwise, set variable at index.
+*
+      IF (IND.LE.0) THEN
+        IND = VCNT + 1
+        NAMES(IND)  = N
+        VALUES(IND) = V
+        VCNT = IND
+      ELSE
+        VALUES(IND) = V
+      END IF
+*
+  998 FORMAT(A,A)
+  999 FORMAT(A,I6)
+      END SUBROUTINE SETVAL
+************************************************************************
+* VALAT: Gets variable value at a given index.
+************************************************************************
+      SUBROUTINE VALAT(IND, BUFF)
+      IMPLICIT NONE
+*     ------------------------------------------------------------------
+      INTEGER*4 MAXVCNT, MAXNSZ, MAXVSZ, VCNT
+      PARAMETER (MAXVCNT=128, MAXNSZ=8, MAXVSZ=128)
+      CHARACTER*(MAXNSZ) NAMES(MAXVCNT)
+      CHARACTER*(MAXVSZ) VALUES(MAXVCNT)
+      COMMON /VARS/ VCNT, NAMES, VALUES
+*     ------------------------------------------------------------------
+      INTEGER IND
+      CHARACTER*(*) BUFF
+      IF (IND.LE.VCNT) THEN
+        BUFF = VALUES(IND)(:LEN_TRIM(VALUES(IND)))
+      ELSE
+        BUFF = ''
+      END IF
+      END SUBROUTINE VALAT
+************************************************************************
+* GETVAL: Gets a variable value (character) related to a name.
+************************************************************************
+* NAME  - I - Variable name
+* VALUE - O - Variable value
+************************************************************************
+* Gets variable value.  Variable names are case INSENSITIVE.  Variable
+* values are not altered by case.  Values are, adjusted left.
+************************************************************************
+      SUBROUTINE GETVAL(NAME, VALUE)
+      IMPLICIT NONE
+*
+      INTEGER IND, VARIND
+      CHARACTER*(*) NAME, VALUE
+      IND = VARIND(NAME)
+      IF (IND.GT.0) THEN
+        CALL VALAT(IND, VALUE)
+      ELSE
+        VALUE = ''
+      END IF
+      END SUBROUTINE GETVAL
+************************************************************************
+* SETPAR: SET a name/value PAIR variable given 'name=value' string.
+************************************************************************
+* PAIR - I - VARIABLE=VALUE pair example: 'pi=3.14'
+************************************************************************
+      SUBROUTINE SETPAR(PAIR)
+      IMPLICIT NONE
+      CHARACTER*(*) PAIR
+      INTEGER*4 I
+      I = INDEX(PAIR, '=')
+      IF (I.GT.0) THEN
+        CALL SETVAL(PAIR(1:I-1), PAIR(I+1:LEN_TRIM(PAIR)))
+      END IF
+      END SUBROUTINE SETPAR
+************************************************************************
+* CHCASE: CHange CASE of string to upper or lower.
+************************************************************************
+* STR - I/O - Character data to be converted to upper/lower case.
+* TOUPR - I - .TRUE. if data is to be converted to upper case.
+************************************************************************
+* Converts character data to upper or lower case.
+*
+* The difference between the character values of lower and upper
+* case.  In ASCII, A < a, in EBCDIC, A > a.  So, if this case is
+* ASCII, DIFF will be a negative number, and converting from lower
+* to upper will subtract the magnitude of the offset.
+************************************************************************
+      SUBROUTINE CHCASE(STR,TOUPR)
+      IMPLICIT NONE
+      CHARACTER*(*) STR
+      CHARACTER C
+      LOGICAL TOUPR
+      INTEGER I,DIFF
+      DIFF = ICHAR('A') - ICHAR('a')
+      DO 1001 I = 1, LEN_TRIM(STR)
+      C = STR(I:I)
+      IF (TOUPR) THEN
+        IF(C.ge.'a'.and.C.le.'z') STR(I:I) = CHAR(ICHAR(C)+DIFF)
+      ELSE
+        IF(C.ge.'A'.and.C.le.'Z') STR(I:I) = CHAR(ICHAR(C)-DIFF)
+      ENDIF
+ 1001 CONTINUE
+      END SUBROUTINE CHCASE
+************************************************************************
+* LCASE: Lower CASE.
+************************************************************************
+* STR - I/O - Character data to be converted to upper/lower case.
+************************************************************************
+* Converts character data to lower case.  Wraps CHGCASE
+************************************************************************
+      SUBROUTINE LCASE(STR)
+      IMPLICIT NONE
+      CHARACTER*(*) STR
+      CALL CHCASE(STR, .FALSE.)
+      END SUBROUTINE LCASE
+************************************************************************
+* UCASE: Upper CASE.
+************************************************************************
+* STR - I/O - Character data to be converted to upper/lower case.
+************************************************************************
+* Converts character data to upper case.  Wraps CHGCASE
+************************************************************************
+      SUBROUTINE UCASE(STR)
+      IMPLICIT NONE
+      CHARACTER*(*) STR
+      CALL CHCASE(STR, .TRUE.)
+      END SUBROUTINE UCASE
 ************************************************************************
